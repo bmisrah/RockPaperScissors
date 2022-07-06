e389eb56cea031cbcfede01730193b01e77d423d
Assana <bmisrah@gmail.com>
2022-07-06 15:19:36 +0000

    third commit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice;
+
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputeChoice();
+  })
+);
+
+function generateComputeChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNumber);
+}
