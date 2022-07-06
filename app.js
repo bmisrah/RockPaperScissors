8a6aea321ae410da8e15f763cc5ec6d3d8a8fbfd
Assana <bmisrah@gmail.com>
2022-07-06 15:59:46 +0000

    correction

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ function getResult() {
   }
   if (computerChoice === "scissors" && userChoice === "paper") {
     result = "you lost!";
-
-    resultDisplay.innerHTML = result;
   }
+  resultDisplay.innerHTML = result;
 }
